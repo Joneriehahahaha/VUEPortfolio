a271cabc23b9fb0c6490e7e7d259ba49c2ee10ea
Jonerie 123 hehehe <154795227+Joneriehahahaha@users.noreply.github.com>
2024-04-03 19:13:01 +0800

    Add files via upload

---
+++ src/components/Contact.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="bg-slate-900">
+
+    <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+      
+        <h2 class="text-3xl font-extrabold text-black">Contact Information</h2>
+        <p class="mt-4 text-lg text-gray-700 font-bold">
+          .
+        </p>
+        
+      
+      
+      
+            
+         
+            <div class="flex justify-center"></div>
+            <div class="max-w-full mx-auto rounded-lg overflow-hidden">
+              <div class="px-6 py-4">
+                <h3 class="text-lg font-bold text-black-400">My Address</h3>
+                <p class="mt-1 text-gray-800">
+                  Purok Sambag Day-as, Cordova, Cebu
+                </p>
+
+                
+              </div>
+              <div class="border-t border-gray-200 px-6 py-4">
+                <h5 class="text-lg font-bold text-black-400">Social Links</h5>
+                <p class="mt-1 text-black font-bold">
+                  Facebook:
+                  <span class="font-normal bold"
+                    >https://www.facebook.com/j.020720</span
+                  >
+                </p>
+                <p class="mt-1 text-black font-bold">
+                  Instagram:
+                  <span class="font-normal bold">https://www.instagram.com/jn.rein/</span>
+                </p>
+                <p class="mt-1 ttext-black font-bold">
+                  Twitter: <span class="font-normal">https://twitter.com/jon.cute/</span>
+                </p>
+              </div>
+              <div class="border-t border-gray-200 px-6 py-4">
+                <h3 class="text-lg text-black font-semibold font-sans">
+                  Contact
+                </h3>
+                <p class="mt-1 text-black font-bold">
+                  Email: <span class="font-normal">jonerietabotabo@gmail.com</span>
+                </p>
+                <p class="mt-1 text-black font-bold">
+                  Phone: <span class="font-normal"> + 639662540485</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
